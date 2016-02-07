287635192eb56b4c3a73555ff84357b431445912
Kevin Vollmer <works.kvollmer@gmail.com>
2016-02-07 15:53:59 -0500

    make old code less shit

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
-#include "stdio.h"
-#include "ncurses.h"
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <ncurses.h>
 #include <unistd.h>
 #include <time.h>
 #include <wchar.h>
-#include "locale.h"
-#include "stdlib.h"
+#include <locale.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "uthash.h"
 
 typedef struct cell {
@@ -12,40 +15,41 @@ typedef struct cell {
   bool status;
   bool brailled;
 } cell;
+
 typedef struct block {
   bool status;
   char bit[9];
 } block;
-block** blocks;
-cell** ccells;
-cell** ncells;
-int col = 0;
-int row = 0;
-int brow = 0;
-int bcol = 0;
 
-struct my_struct {
-    char* id;
-    wchar_t name;
-    UT_hash_handle hh;
-};
+static block** blocks;
+static cell** ccells;
+static cell** ncells;
+static int col = 0;
+static int row = 0;
+static int brow = 0;
+static int bcol = 0;
 
-struct my_struct *users = NULL;
+typedef struct {
+  char* id;
+  wchar_t name;
+  UT_hash_handle hh;
+} braille_cell;
 
-void add_user(char* key, wchar_t value) {
-    struct my_struct *s;
+static braille_cell *braille_set = NULL;
 
-    s = (struct my_struct*)malloc(sizeof(struct my_struct));
-    s->id = strdup(key);
-    s->name = value;
-    HASH_ADD_KEYPTR(hh, users, s->id, strlen(s->id), s);
+braille_cell* find_user(char* key)
+{
+  braille_cell *bc;
+  HASH_FIND_STR(braille_set, key, bc);
+  return bc;
 }
 
-struct my_struct* find_user(char* key) {
-    struct my_struct *s;
-
-    HASH_FIND_STR( users, key, s );
-    return s;
+void add_braille(char* key, wchar_t value)
+{
+  braille_cell *bc = malloc(sizeof(braille_cell));
+  bc->id = strdup(key);
+  bc->name = value;
+  HASH_ADD_STR(braille_set, id, bc);
 }
 
 void gen_table()
@@ -56,14 +60,12 @@ void gen_table()
 int countblock_dead(int r, int c)
 {
   int count = 0;
-  for (int i=r-1; i<r+2; i++)
-    for (int j=c-1; j<c+2; j++)
-  {
-    if (i >= 0 && j >= 0 &&
-        i < row && j < col)
-    {
-      if (ccells[i][j].alive)
-        count++;
+  for (int i=r-1; i<r+2; i++) {
+    for (int j=c-1; j<c+2; j++) {
+      if (i >= 0 && j >= 0 && i < row && j < col) {
+        if (ccells[i][j].alive)
+          count++;
+      }
     }
   }
   return count-1;
@@ -72,24 +74,19 @@ int countblock_dead(int r, int c)
 int countblock_alive(int r, int c)
 {
   int count = 0;
-  for (int i=r-1; i<r+2; i++)
-    for (int j=c-1; j<c+2; j++)
-  {
-    if (i >= 0 && j >= 0 &&
-        i < row && j < col)
-    {
-      if (!ccells[i][j].alive)
-      {
-        if (!ncells[i][j].status)
-        {
-          if (countblock_dead(i,j) == 2)
-          {
-            ncells[i][j].alive = true;
+  for (int i=r-1; i<r+2; i++) {
+    for (int j=c-1; j<c+2; j++) {
+      if (i >= 0 && j >= 0 && i < row && j < col) {
+        if (!ccells[i][j].alive) {
+          if (!ncells[i][j].status) {
+            if (countblock_dead(i,j) == 2) {
+              ncells[i][j].alive = true;
+            }
           }
         }
+        else
+          count++;
       }
-      else
-        count++;
     }
   }
   return count-1;
@@ -97,19 +94,14 @@ int countblock_alive(int r, int c)
 
 void draw()
 {
-  for (int i=0; i<brow; i++)
-  {
+  for (int i=0; i<brow; i++) {
     for (int j=0; j<bcol; j++)
-    {
       mvprintw(i,j, "%lc", find_user(blocks[i][j].bit)->name);
-    }
   }
-  for (int i=0; i<row; i++)
-  {
-    for (int j=0; j<col; j++)
-    {
-      if (ccells[i][j].alive)
-      {
+
+  for (int i=0; i<row; i++) {
+    for (int j=0; j<col; j++) {
+      if (ccells[i][j].alive) {
         int b = countblock_alive(i,j);
         if (b == 3 || b == 2)
           ncells[i][j].alive = true;
@@ -121,19 +113,16 @@ void draw()
 void set_braille_nodes(int r, int c, int a, int b)
 {
   int bit = 0;
-  for (int i=r; i<r+4; i++)
-    for (int j=c; j<c+2; j++)
-  {
-    if (i >= 0 && j >= 0 &&
-        i < row && j < col)
-    {
-      if (ccells[i][j].alive)
-      {
-        blocks[a][b].bit[bit] = '1';
+  for (int i=r; i<r+4; i++) {
+    for (int j=c; j<c+2; j++) {
+      if (i >= 0 && j >= 0 && i < row && j < col) {
+        if (ccells[i][j].alive)
+          blocks[a][b].bit[bit] = '1';
+
+        ccells[i][j].brailled = true;
       }
-      ccells[i][j].brailled = true;
+      bit++;
     }
-    bit++;
   }
 }
 
@@ -143,20 +132,16 @@ void braillify()
   int c = 0;
   int a = 0;
   int b = 0;
-  for (int i=0;i<row;i++)
-  {
-    for (int j=0;j<col;j++)
-    {
-        set_braille_nodes(i,j,r,c);
-      if (b > 1)
-      {
+  for (int i=0;i<row;i++) {
+    for (int j=0;j<col;j++) {
+      set_braille_nodes(i,j,r,c);
+      if (b > 1) {
         b = 0;
         c++;
       }
       b++;
     }
-    if ( a > 3)
-    {
+    if ( a > 3) {
       a = 0;
       r++;
     }
@@ -167,10 +152,8 @@ void braillify()
 
 void copy_cycle()
 {
-  for (int i=0;i<row;i++)
-  {
-    for (int j=0;j<col;j++)
-    {
+  for (int i=0;i<row;i++) {
+    for (int j=0;j<col;j++) {
       ccells[i][j].alive = ncells[i][j].alive;
       ccells[i][j].status = ncells[i][j].status;
     }
@@ -179,27 +162,22 @@ void copy_cycle()
 
 void reset_cycle()
 {
-  for (int i=0;i<row;i++)
-  {
-    for (int j=0;j<col;j++)
-    {
+  for (int i=0;i<row;i++) {
+    for (int j=0;j<col;j++) {
       ncells[i][j].alive = false;
       ncells[i][j].brailled = false;
       ncells[i][j].status = false;
     }
   }
-  for (int i=0;i<brow;i++)
-  {
-    for (int j=0;j<bcol;j++)
-    {
+
+  for (int i=0;i<brow;i++) {
+    for (int j=0;j<bcol;j++) {
       blocks[i][j].status = false;
-      for (int n=0; n<8; n++)
-      {
+      for (int n=0; n<8; n++) {
         blocks[i][j].bit[n] = '0';
       }
     }
   }
-  return;
 }
 
 void create()
@@ -207,14 +185,12 @@ void create()
   ccells = (cell**)malloc(row * sizeof(cell*));
   ncells = (cell**)malloc(row * sizeof(cell*));
   blocks = (block**)malloc(brow * sizeof(block*));
-  for (int i=0;i<row;i++)
-  {
+  for (int i=0;i<row;i++) {
     ccells[i] = (cell*)malloc(col * sizeof(cell));
     ncells[i] = (cell*)malloc(col * sizeof(cell));
   }
   for (int i=0;i<brow;i++)
     blocks[i] = (block*)malloc(bcol * sizeof(block));
-  return;
 }
 
 int main(int argc, char** argv)
@@ -236,28 +212,23 @@ int main(int argc, char** argv)
   bcol = col;
   row*=4;
   col*=2;
-  fprintf(stderr, "row, col %d, %d\n", row, col);
   gen_table();
   create();
 
-  while(1)
-  {
+  for (int c=0;c<col*200;c++) {
+    int y = rand() % row;
+    int x = rand() % col;
+    ccells[y][x].alive = true;
+  }
 
-    for (int c=0;c<col;c++)
-    {
-      int y = rand() % row;
-      int x = rand() % col;
-      ccells[y][x].alive = true;
-    }
+  while(1) {
     reset_cycle();
     braillify();
     draw();
     refresh();
     copy_cycle();
-    fprintf(stderr, "==============\n");
   }
-  for (int i=0;i<row;i++)
-  {
+  for (int i=0;i<row;i++) {
     free(ccells[i]);
     free(ccells[i]);
   }
